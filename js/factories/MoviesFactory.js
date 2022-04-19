d8e219d7a2b9891346e93a360432b6fe128c0fc6
VGrolleau <virginie.grolleau+github@protonmail.com>
2022-04-19 12:25:03 +0200

    Exercise

--- js/factories/MoviesFactory.js
+++ js/factories/MoviesFactory.js
@@ -3,10 +3,13 @@ class MoviesFactory {
         // Si le type correspond à l'ancienne API, alors retourne moi l'ancien formattage
         if (type === 'oldApi') {
             return new OldMovie(data)
-        // Sinon retourne moi le nouveau formattage
+                // Sinon retourne moi le nouveau formattage
         } else if (type === 'newApi') {
             return new Movie(data)
-        // Une bonne pratique est de throw une erreur si le format n'est pas reconnu
+                // Sinon retourne moi le formattage externe
+        } else if (type === 'externalApi') {
+            return new ExternalMovie(data)
+                // Une bonne pratique est de throw une erreur si le format n'est pas reconnu
         } else {
             throw 'Unknown type format'
         }
